8531127f6b5f28e386a8535af396a618d964f460
Karthik <karthik.krishnaswamy17@gmail.com>
2021-10-12 20:40:37 +0530

    first commit

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,21 @@
+#!/usr/bin/env groovy
+package com.example
+
+def script
+
+class Docker implements Serializable{
+    Docker(script){
+        this.script=script
+    }
+
+    def buildDockerImage(String imageName){
+        script.echo "Building Image"
+        script.withCredentials([script.scriptusernamePassword(credentialsId:'docker-hub-account',usernameVariable:'user',passwordVariable:'pass')])
+                {
+                    script.sh "docker build -t $script.{imageName} ."
+                    script.sh "echo $script.pass |docker login --username $script.user --password-stdin"
+                    script.sh "docker push $script.x`{imageName}"
+                }
+    }
+
+}
