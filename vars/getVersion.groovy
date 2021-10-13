6f7a73be52f6a0242b7d43b162588dcea97739f9
Karthik <karthik.krishnaswamy17@gmail.com>
2021-10-13 17:32:22 +0530

    Udpates

--- vars/getVersion.groovy
+++ vars/getVersion.groovy
@@ -3,7 +3,8 @@ import com.example.Docker
 
 @NonCPS
 def call (){
-  def m = readFile('pom.xml') =~ '<version>(.+)</version>'
+  def m ="Hello"
+  // def m = readFile('pom.xml') =~ '<version>(.+)</version>'
   return m
  // def version= m[0][1]
  // env.IMAGE_NAME= "$version-$BUILD_NUMBER"
