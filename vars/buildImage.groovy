1f2c7ee483fef60f7348651aaaf2e873ca6a9db8
Karthik <karthik.krishnaswamy17@gmail.com>
2021-10-13 17:28:01 +0530

    updates

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 import com.example.Docker
 
+
 def call (String imageName){
+  
     return new Docker(this).buildDockerImage(imageName)
 
 }
---
+++ vars/getVersion.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+import com.example.Docker
+
+@NonCPS
+def call (){
+  def m = readFile('pom.xml') =~ '<version>(.+)</version>'
+  return m
+ // def version= m[0][1]
+ // env.IMAGE_NAME= "$version-$BUILD_NUMBER"
+}
+
+  
